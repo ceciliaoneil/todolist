19b97bc79fd7ef3bc26f40162e9b3c77e2819ecd
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-10-06 18:24:06 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+// create your first component
+const Home = () => { // declaracion de estados
+
+    const [tarea, setTarea] = useState("")
+    const [listaDeTareas, setListadeTareas] = useState([])
+
+
+    function handleSubmit(e) {
+
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            setListadeTareas([
+                ...listaDeTareas,
+                tarea
+            ])
+            setTarea("");
+            console.log(listaDeTareas)
+        }
+
+
+    }
+
+    const eliminarTarea = (i) => {
+        const tareaFiltrada = listaDeTareas.filter((tarea, index) => {
+            if (i !== index) {
+                return tarea
+            }
+            });
+            setListadeTareas(tareaFiltrada)
+    };
+
+
+    
+    return (
+        <div className="container m-5">
+            <div className="card p-5">
+                <form>
+                    <div className="mb-3">
+
+                        <h1>To do List</h1>
+
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            onChange={
+                                (e) => setTarea(e.target.value)
+                            }
+                            onKeyDown={handleSubmit}
+                            value={tarea}/>
+                    </div>
+                </form>
+
+
+                <ul className="list-group list-group-flush">
+                    {
+                    listaDeTareas.map((tarea, i) => {
+                        return (
+                            <li className="list-group-item border border-1 tareaOculta"
+                                key={i}>
+                                {tarea}
+                                <span className="close"
+                                    onClick={
+                                        () => eliminarTarea(i)
+                                }>x</span>
+                            </li>
+                        );
+                    })
+                } </ul>
+
+                <div className="p-1"> {listaDeTareas.length == 0 ? "No hay tareas, añadir tareas" : "Numero de tareas: "+listaDeTareas.length} </div>
+
+            </div>
+        </div>
+    );
 };
 
 export default Home;
